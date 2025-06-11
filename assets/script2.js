8945a911bd3ab11becc942e1da82fceb3306352c
John Christotle Agholor <christotleagholor@gmail.com>
2025-06-11 22:37:19 +0100

    Add files via upload

---
+++ assets/script2.js
@@ -0,0 +1,56 @@
+
+
+// Meta Pixel Code
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1355443531781813');
+fbq('init', '1472549230151144');
+  fbq('init', '1616156851915421');
+  fbq('init', '741903313649102');
+  fbq('init', '6278111178968810');
+  fbq('init', '857685435913343');
+    fbq('init', '2268578353517458');
+fbq('track', 'PageView');
+
+
+// Inject <noscript> tags dynamically
+(function() {
+    var noscriptTag1 = document.createElement('noscript');
+    noscriptTag1.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1355443531781813&ev=PageView&noscript=1"/>';
+    
+    var noscriptTag2 = document.createElement('noscript');
+    noscriptTag2.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1472549230151144&ev=PageView&noscript=1"/>';
+       
+    var noscriptTag3 = document.createElement('noscript');
+    noscriptTag3.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1616156851915421&ev=PageView&noscript=1"/>';
+    
+       
+    var noscriptTag4 = document.createElement('noscript');
+    noscriptTag4.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=741903313649102&ev=PageView&noscript=1"/>';
+    
+       
+    var noscriptTag5 = document.createElement('noscript');
+    noscriptTag5.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=6278111178968810&ev=PageView&noscript=1"/>';
+    
+       
+    var noscriptTag6 = document.createElement('noscript');
+    noscriptTag6.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=857685435913343&ev=PageView&noscript=1"/>';
+    
+       var noscriptTag7 = document.createElement('noscript');
+    noscriptTag7.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2268578353517458&ev=PageView&noscript=1"/>';
+    
+    document.body.appendChild(noscriptTag1);
+    document.body.appendChild(noscriptTag2);
+        document.body.appendChild(noscriptTag3);
+            document.body.appendChild(noscriptTag4);
+                document.body.appendChild(noscriptTag5);
+                    document.body.appendChild(noscriptTag6);
+                    document.body.appendChild(noscriptTag7);
+})();
